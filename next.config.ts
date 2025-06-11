63fa34e0c0495e37742d7eda67eed35c697369ad
ivilla15 <ivilla15@calstatela.edu>
2025-06-11 11:12:15 -0700

    feat: add overview section with all relevant info and images. Change image in social change model section.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-      {protocol: 'https', hostname: 'www.calstatelausu.org', pathname: '/_next/image'},
+      {
+        protocol: "https",
+        hostname: "www.calstatelausu.org",
+        pathname: "/_next/image",
+      },
+      {
+        protocol: "https",
+        hostname: "bubqscxokeycpuuoqphp.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import {
   CallToAction,
   Events,
   Hero,
+  Overview,
   Promotions,
   Testimonial,
   Values,
 } from "./modules";
+
+const x = "hello"
 
 export default function Home() {
   return (
@@ -14,6 +17,7 @@ export default function Home() {
       <main>
         <Hero />
         <Testimonial />
+        <Overview />
         <Benefits />
         <Events />
         <Values />
